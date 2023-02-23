a9688f85a8a8ee9df685aa14103bea7a7e85963f
CarlosRuiz <carua97@gmail.com>
2023-02-23 11:45:00 +0100

    Reducer modifies

--- src/components/action/action.tsx
+++ src/components/action/action.tsx
@@ -1,8 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import * as ac from "../../reducer/phone.actions.creator";
 export function Action({ type }: any) {
+  const handlerIsCalling = (isCalling: boolean) => {
+    dispatch(ac.changeCallCreator(isCalling));
+  };
+
   return (
     <>
-      <a href="#" className={type}>
+      <a
+        href="#"
+        className={type}
+        onClick={() => {
+          handlerIsCalling(true);
+        }}
+      >
         {type}
       </a>
       <a href="#" className="hang active">
@@ -10,4 +21,7 @@ export function Action({ type }: any) {
       </a>
     </>
   );
+}
+function dispatch(arg0: any) {
+  throw new Error("Function not implemented.");
 }
--- src/reducer/phone.reducer.test.tsx
+++ src/reducer/phone.reducer.test.tsx
@@ -7,7 +7,13 @@ import { phoneReducer } from "./phone.reducer";
 describe("Given the phoneReducer", () => {
   describe("When we set an initial state on the action", () => {
     test("Then if it clicks on a number it should be a change state", () => {
-      let result = phoneReducer("", clickNumberCreator("8"));
+      let result = phoneReducer(
+        {
+          phoneNumber: "",
+          isCalling: false,
+        },
+        clickNumberCreator("8")
+      );
       expect(result).toBe("8");
     });
     test("Then if it clicks on delete, it should clean the state", () => {
