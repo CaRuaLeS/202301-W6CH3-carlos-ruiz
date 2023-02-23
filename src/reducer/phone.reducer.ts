547f8c783ac9cd18e04fcfce037f95ae4118a031
CarlosRuiz <carua97@gmail.com>
2023-02-23 10:40:54 +0100

    Add reducer hang and call

--- src/reducer/phone.reducer.ts
+++ src/reducer/phone.reducer.ts
@@ -1,13 +1,33 @@
 import { createReducer } from "@reduxjs/toolkit";
 import * as ac from "./phone.actions.creator";
 
-const initialState: string = "";
+type state = {
+  phoneNumber: string;
+  isCalling: boolean;
+};
+
+const initialState: state = {
+  phoneNumber: "",
+  isCalling: false,
+};
 
 export const phoneReducer = createReducer(initialState, (builder) => {
-  builder.addCase(
-    ac.clickNumberCreator,
-    (state, { payload }) => state + payload
-  );
-  builder.addCase(ac.deleteNumberCreator, (state) => (state = ""));
+  builder.addCase(ac.clickNumberCreator, (state, { payload }) => ({
+    ...state,
+    phoneNumber: state.phoneNumber + payload,
+  }));
+  builder.addCase(ac.deleteNumberCreator, (state) => ({
+    ...state,
+    phoneNumber: initialState.phoneNumber,
+  }));
+  builder.addCase(ac.callCreator, (state) => ({
+    ...state,
+    isCalling: (state.isCalling = true),
+  }));
+  builder.addCase(ac.hangCreator, (state) => ({
+    ...state,
+    isCalling: (state.isCalling = false),
+  }));
+
   builder.addDefaultCase((state) => state);
 });
